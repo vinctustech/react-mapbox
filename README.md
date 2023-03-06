34508c266b0f6dbac3f2cc268b631e4fbc52b3ee
Edward Adrian Maxedon <edadma@gmail.com>
2023-03-06 09:58:36 -0500

    SC-581 update dependencies

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 Typescript library.
 
 Documentation can be found [here](https://vinctustech.github.io/react-mapbox/).
+
+Testing
+-------
+There is a test program in the `test` directory. It gets the Mapbox access token from environment variable
+`REACT_APP_MAPBOX_API_KEY` which can be defined in `.env.development`.
+
+To start the test program type:
+1. ```
+   npm i```
+2. ```
+   tsc```
+3. ```
+   cd test```
+4. ```
+   npm i```
+5. ```
+   npm start```
--- test/src/index.tsx
+++ test/src/index.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 )
